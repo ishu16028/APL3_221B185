9c1e84a870b2f34253df49990b542920248270f5
221B185 <221B185@juet.co.in>
2025-03-25 12:31:16 +0530

    Lab 5

--- LE5/Main.java
+++ LE5/Main.java
@@ -1,7 +1,41 @@
-*******************************************************************************/
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+class Beer extends Beverage{
+    public void addCondiments(){
+        System.out.println("Add nothing.");
+    }
+}
+
+abstract class Beverage{
+
+    protected void pourBeverage(int qnt){
+        System.out.println("Pour " + qnt + " of beverage into a glass.");
+    }
+    protected abstract void addCondiments();
+    protected void stir(){}
+    private void serve(){
+        System.out.println("Serve through waiter.");
+    }
+    final public void templateMethod(int qnt){
+        pourBeverage(qnt);
+        addCondiments();
+        stir();
+        serve();
+    }
+}
+
+class Whisky extends Beverage{
+    public void addCondiments(){
+        System.out.println("Add soda.");
+    }
+    public void stir(){
+        System.out.println("Stir for 30 seconds.");
+    }
+}
+
+class Main{
+    public static void main(String args[]){
+        Beverage b = new Whisky();
+        b.templateMethod(30);
+        b = new Beer();
+        b.templateMethod(200);
+    }
 }
